9612951a428a4f3841e482952cb448523739aa80
Kushal Pisavadia <kushi.p@gmail.com>
2009-12-30 22:16:59 +0000

    closed off the footer tag, which wasn't checked previously

--- footer.php
+++ footer.php
@@ -9,7 +9,7 @@
 			<p><?php bloginfo('name'); ?> is proudly powered by <a href="http://wordpress.org/">WordPress <?php bloginfo('version'); ?></a> <a href="<?php bloginfo('rss2_url'); ?>">Entries (RSS)</a> <a href="<?php bloginfo('comments_rss2_url'); ?>">Comments (RSS)</a>. <!-- <?php echo get_num_queries(); ?> queries. <?php timer_stop(1); ?> seconds. --></p>
 	
 			<?php wp_footer(); ?>
-		<footer>
+		</footer>
 
 	</body>
 
