c840df464147cdc0f46451c03157d91414d86b5b
Kushal Pisavadia <kushi.p@gmail.com>
2009-12-30 21:49:53 +0000

    changed doctype to use html5 format and removed buggy content-type declarations

--- header.php
+++ header.php
@@ -5,11 +5,11 @@
  */
 ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
 
-	<head profile="http://gmpg.org/xfn/11">
-		<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+	<head>
+		<meta charset="<?php bloginfo('charset'); ?>" />
 		<title><?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
 		<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
 		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
@@ -18,6 +18,7 @@
 	</head>
 	
 	<body <?php body_class(); ?>>
-		
-		<h1><a href="<?php echo get_option('home'); ?>/"><?php bloginfo('name'); ?></a></h1>
-		<p><?php bloginfo('description'); ?></p>
+		<header>
+			<h1><a href="<?php echo get_option('home'); ?>/"><?php bloginfo('name'); ?></a></h1>
+			<p><?php bloginfo('description'); ?></p>
+		</header>
